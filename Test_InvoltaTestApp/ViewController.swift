9352257e686c2c391a8b4050efe23a4ba7b50616
DevBorisElkin <ea.foreverrr@gmail.com>
2022-08-26 15:06:03 +0300

    Initial commit

---
+++ Test_InvoltaTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_InvoltaTestApp
+//
+//  Created by test on 26.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
